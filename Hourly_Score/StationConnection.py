371e15ec66145d98b887a16722b530a4412b6899
Zhe Sun <injure21@gmail.com>
2025-10-13 20:23:21 -0400

    Update StationConnection.py

--- Hourly_Score/StationConnection.py
+++ Hourly_Score/StationConnection.py
@@ -72,47 +72,58 @@ class StationConnectionProcessor:
         self.output_path = output_path.rstrip("/")
         self.date_str = date_str
         self.hour_str = hour_str
+        self.stationarity_path = f"/user/ZheS/wifi_score_v4/stationarity/{self.date_str}"
 
     def compute_stationarity(self):
-        """Step 1: Compute stationarity (daily level)."""
-        df_sh = self.spark.read.parquet(f"{self.input_path}/date={(datetime.strptime(self.date_str, '%Y%m%d') - timedelta(days=1)).strftime('%Y%m%d')}")
 
-        df = (
-            df_sh.withColumn("signal_strength", F.col("Station_Data_connect_data.signal_strength"))
-            .filter(F.col("signal_strength").isNotNull())
-            .select("Tplg_Data_model_name", "rowkey", "station_data_connect_data.*")
-            .withColumn("sn", F.regexp_extract("rowkey", r"-([A-Z0-9]+)_", 1))
-        )
+        try:
+            # 2. Attempt to READ the existing data
+            print("Attempting to read pre-computed stationarity data...")
+            stationary_daily_df = self.spark.read.parquet(self.stationarity_path)
+            print("Successfully read existing stationary data.")
+            return stationary_daily_df
+        except Exception as e:
+            print(f"Failed to read existing stationary data: {e}")
 
-        # Window per (sn, station_mac)
-        window_spec = Window().partitionBy("sn", "station_mac")
+            df_sh = self.spark.read.parquet(f"{self.input_path}/date={(datetime.strptime(self.date_str, '%Y%m%d') - timedelta(days=1)).strftime('%Y%m%d')}")
 
-        # Percentiles
-        p3 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[0]})').over(window_spec)
-        p10 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[1]})').over(window_spec)
-        p50 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[2]})').over(window_spec)
-        p90 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[3]})').over(window_spec)
-
-        df_outlier = (
-            df.withColumn('3%_val', p3)
-              .withColumn('10%_val', p10)
-              .withColumn('50%_val', p50)
-              .withColumn('90%_val', p90)
-              .withColumn("lower_bound", F.col('10%_val') - 2 * (F.col('90%_val') - F.col('10%_val')))
-              .withColumn("outlier", F.when(F.col("lower_bound") < F.col("3%_val"), F.col("lower_bound")).otherwise(F.col("3%_val")))
-              .filter(F.col("signal_strength") > F.col("outlier"))
-        )
-
-        stationary_daily_df = (
-            df_outlier.withColumn("diff", F.col('90%_val') - F.col('50%_val'))
-            .withColumn("stationarity", F.when(F.col("diff") <= 5, F.lit("1")).otherwise(F.lit("0")))
-            .groupby("sn", "station_mac").agg(F.max("stationarity").alias("mobility_status"))
-            .withColumn(
-                "mobility_status",
-                F.when(F.col("mobility_status") == 1, F.lit("Stationary")).otherwise(F.lit("Non-stationary"))
+            df = (
+                df_sh.withColumn("signal_strength", F.col("Station_Data_connect_data.signal_strength"))
+                .filter(F.col("signal_strength").isNotNull())
+                .select("Tplg_Data_model_name", "rowkey", "station_data_connect_data.*")
+                .withColumn("sn", F.regexp_extract("rowkey", r"-([A-Z0-9]+)_", 1))
             )
-        )
-        return stationary_daily_df
+
+            # Window per (sn, station_mac)
+            window_spec = Window().partitionBy("sn", "station_mac")
+
+            # Percentiles
+            p3 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[0]})').over(window_spec)
+            p10 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[1]})').over(window_spec)
+            p50 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[2]})').over(window_spec)
+            p90 = F.expr(f'percentile_approx(signal_strength, {self.PERCENTILES[3]})').over(window_spec)
+
+            df_outlier = (
+                df.withColumn('3%_val', p3)
+                .withColumn('10%_val', p10)
+                .withColumn('50%_val', p50)
+                .withColumn('90%_val', p90)
+                .withColumn("lower_bound", F.col('10%_val') - 2 * (F.col('90%_val') - F.col('10%_val')))
+                .withColumn("outlier", F.when(F.col("lower_bound") < F.col("3%_val"), F.col("lower_bound")).otherwise(F.col("3%_val")))
+                .filter(F.col("signal_strength") > F.col("outlier"))
+            )
+
+            stationary_daily_df = (
+                df_outlier.withColumn("diff", F.col('90%_val') - F.col('50%_val'))
+                .withColumn("stationarity", F.when(F.col("diff") <= 5, F.lit("1")).otherwise(F.lit("0")))
+                .groupby("sn", "station_mac").agg(F.max("stationarity").alias("mobility_status"))
+                .withColumn(
+                    "mobility_status",
+                    F.when(F.col("mobility_status") == 1, F.lit("Stationary")).otherwise(F.lit("Non-stationary"))
+                )
+            )
+            stationary_daily_df.write.mode("overwrite").parquet(self.stationarity_path)
+            return stationary_daily_df
 
     def process_hourly(self, stationary_daily_df):
         """Step 2: Process hourly data and join with stationarity."""
@@ -154,14 +165,19 @@ class StationConnectionProcessor:
         final_df = self.process_hourly(stationary_daily_df)
         return final_df
 
+
+
+
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName('Zhe_bhr_wifi_score_hourly_report_v1')\
+    spark = SparkSession.builder.appName('StationConnection_bhr_wifi_score_hourly_report_v1')\
                         .config("spark.ui.port","24045")\
                         .getOrCreate()
 
     
-    date_str = "20250930"
-    hour_str = "15"
+    current_datetime = datetime.now()
+
+    date_str = current_datetime.strftime("%Y%m%d")
+    hour_str = current_datetime.strftime("%H")
     #station_connection_hourly
     hdfs_pd = "hdfs://njbbvmaspd11.nss.vzwnet.com:9000/"
     hdfs_pa =  'hdfs://njbbepapa1.nss.vzwnet.com:9000'
@@ -177,5 +193,4 @@ if __name__ == "__main__":
             hour_str= hour_str
         )
         station_connection_df = processor.run()
-        station_connection_df = spark.read.parquet(f"{hdfs_pa}/sha_data//vz-bhr-athena/reports/station_connection_houry/")
         
